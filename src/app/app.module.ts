170eac48e0d9fea354fb6b9be6ffefa9aa3f045a
jayantsingh16 <jayant.singh@nl.abnamro.com>
2021-05-22 09:43:47 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { SidenavListComponent } from './sidenav-list/sidenav-list.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     MaterialModule
